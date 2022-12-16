97fe89ab4c5d5b2a0c1dae2770375877b051b80a
mackeynichols <mackey.nichols@gmail.com>
2022-12-16 20:46:21 +0000

    PEP8 fixes on plugin.py

--- ckanext/iotrans/plugin.py
+++ ckanext/iotrans/plugin.py
@@ -1,22 +1,23 @@
 import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
-
 from . import iotrans
 
 
 class IotransPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IActions)
-
+    """
     # ==============================
     # IActions
     # ==============================
-    # These are custom api endpoints
-    # ex: hitting <ckan_url>/api/action/extract_info will trigger the api.extract_info function
-    # These can also be used with tk.get_action("extract_info"), for example, in this CKAN extension code
+    These are custom api endpoints
+    ex: hitting <ckan_url>/api/action/to_file will trigger
+    the api.to_file function
+
+    These can also be used with tk.get_action("to_file"),
+    for example, in this CKAN extension code
+    """
+    plugins.implements(plugins.IActions)
 
     def get_actions(self):
         return {
             "to_file": iotrans.to_file,
             "prune": iotrans.prune,
         }
-
